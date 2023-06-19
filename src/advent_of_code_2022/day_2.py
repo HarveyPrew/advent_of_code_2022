ff56a06eb9de96ca712c03994d35b66cc52d0e87
HarveyPrew <harvey.prewer@gmail.com>
2023-06-19 22:08:18 +0100

    scores for moves are working

--- src/advent_of_code_2022/day_2.py
+++ src/advent_of_code_2022/day_2.py
@@ -1,2 +1,21 @@
 def get_file(path):
     return open(path, "r")
+
+
+def point_receiver(path):
+    score = 0
+
+    with get_file(path) as file:
+        for line in file:
+            move = line.strip()
+
+            if "X" in move:
+                score += 1
+
+            if "Y" in move:
+                score += 2
+
+            if "Z" in move:
+                score += 3
+
+    return score
--- test/test_day2.py
+++ test/test_day2.py
@@ -1,6 +1,12 @@
-from advent_of_code_2022.day_2 import get_file
+from advent_of_code_2022.day_2 import get_file, point_receiver
 
 
 def test_file_opens():
     opened_file = get_file('input_day_2_test.txt')
     assert opened_file is not None
+
+
+def test_move_score_is_correct():
+    score = point_receiver('input_day_2_test.txt')
+    assert score == 6
+
