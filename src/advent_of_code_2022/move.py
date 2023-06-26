c3954d7ba319cbf1782323111b9b431f218988ac
HarveyPrew <harvey.prewer@gmail.com>
2023-06-26 21:59:45 +0100

    implemented class variables and maps

--- src/advent_of_code_2022/move.py
+++ src/advent_of_code_2022/move.py
@@ -1,4 +1,37 @@
 class Move:
+    OUTCOME_POINTS_MAP = {
+        ("rock", "scissors"): 6,
+        ("paper", "rock"): 6,
+        ("scissors", "paper"): 6,
+        ("rock", "rock"): 3,
+        ("paper", "paper"): 3,
+        ("scissors", "scissors"): 3,
+    }
+
+    OUTCOME_CONVERTER_MAP = {
+        "lose": 0,
+        "draw": 3,
+        "win": 6,
+    }
+
+    MOVE_POINTS_MAP = {
+        "rock": 1,
+        "paper": 2,
+        "scissors": 3,
+    }
+
+    MY_MOVE_MAP = {
+        ("win", "rock"): 2,
+        ("win", "paper"): 3,
+        ("win", "scissors"): 1,
+        ("draw", "rock"): 1,
+        ("draw", "paper"): 2,
+        ("draw", "scissors"): 3,
+        ("lose", "rock"): 3,
+        ("lose", "paper"): 1,
+        ("lose", "scissors"): 2,
+    }
+
     def __init__(self, opponent_move, my_move, outcome):
         self.opponent_move = opponent_move
         self.my_move = my_move
@@ -10,18 +43,10 @@ class Move:
         self.total_points = None
 
     def calculate_outcome_points(self):
-        if self.my_move == "rock" and self.opponent_move == "scissors":
-            return 6
-        elif self.my_move == "paper" and self.opponent_move == "rock":
-            return 6
-        elif self.my_move == "scissors" and self.opponent_move == "paper":
-            return 6
-        elif self.my_move == self.opponent_move:
-            return 3
-        elif self.my_move == 0:
-            return 0
-        else:
-            return 0
+        if self.my_move == self.opponent_move:
+            return Move.OUTCOME_POINTS_MAP.get((self.my_move,
+                                                self.opponent_move), 0)
+        return 0
 
     def calculate_outcome_converter(self):
         if self.outcome == "lose":
