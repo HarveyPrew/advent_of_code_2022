66052336af0d33adcf72df7eb99d0c6709806231
HarveyPrew <harvey.prewer@gmail.com>
2023-06-27 18:35:46 +0100

    refacted + comments

--- src/advent_of_code_2022/day_2.py
+++ src/advent_of_code_2022/day_2.py
@@ -1,50 +1,57 @@
 from advent_of_code_2022.move import Move
 
 
-def move_converter(move, rock, paper, scissors):
-    moves = {
-        rock: "rock",
-        paper: "paper",
-        scissors: "scissors"
+def parseRounds(path):
+    # Creating a list called rounds
+    rounds = list()
+
+    # parse through txt file to collect all the rounds
+    with open(path) as f:
+        for line in f:
+            round = line.strip()
+            rounds.append((round[0], round[2]))
+
+    return rounds
+
+
+def scoreOneRound(round):
+    # Dictionary converting the oppenents move from letters
+    # to assigned variables with given scores
+    rock = 1
+    paper = 2
+    scissors = 3
+    plays = {'A': rock, 'B': paper, 'C': scissors}
+
+    # opponentPlay being the assigned variables
+    opponentPlay = plays[round[0]]
+
+    # Dictionary converting the outcome from letters
+    # to assigned variables with given scores
+    lose = 0
+    draw = 3
+    win = 6
+    outcomes = {'X': lose, 'Y': draw, 'Z': win}
+
+    outcome = outcomes[round[1]]
+
+    # Dictionary of the opponents move and the value being
+    # another dictionary with outcome and my given move.
+    playForOutcome = {
+        rock: {lose: scissors, draw: rock, win: paper},
+        paper: {lose: rock, draw: paper, win: scissors},
+        scissors: {lose: paper, draw: scissors, win: rock},
     }
-    return moves.get(move)
+
+    # Accessing my move for given outcome + opponent move
+    myPlay = playForOutcome[opponentPlay][outcome]
+
+    roundScore = myPlay + outcome
+
+    return roundScore
 
 
-def outcome_converter(move, lose, draw, win):
-    outcomes = {
-        lose: "lose",
-        draw: "draw",
-        win: "win"
-    }
-    return outcomes.get(move)
+def scoreRounds(rounds):
+    # Summing all the scoreOneRound value in the rounds list
+    totalScore = sum(scoreOneRound(round) for round in rounds)
 
-
-def point_receiver(path):
-    score = 0
-
-    with open(path, "r") as file:
-        for line in file:
-            raw_move = line.strip()
-            opponent_move = move_converter(raw_move[0], "A", "B", "C")
-            my_move = move_converter(raw_move[2], "X", "Y", "Z")
-
-            move = Move(opponent_move, my_move)
-            score += move.total_points
-
-    return score
-
-
-def point_receiver_when_outcome_given(path):
-    score = 0
-
-    with open(path, "r") as file:
-        for line in file:
-            raw_move = line.strip()
-            opponent_move = move_converter(raw_move[0], "A", "B", "C")
-            outcome = outcome_converter(raw_move[2], "X", "Y", "Z")
-
-            move = Move(opponent_move, 0, outcome)
-            score += (move.outcome_converter +
-                      move.my_move_when_outcome_given)
-
-    return score
+    return totalScore
--- test/test_day2.py
+++ test/test_day2.py
@@ -1,20 +1,15 @@
-from advent_of_code_2022.day_2 import (point_receiver, point_receiver_when_outcome_given,
-                                       outcome_converter)
+from advent_of_code_2022.day_2 import (parseRounds, scoreRounds)
 from advent_of_code_2022.move import Move
 
 
 def test_point_receiver_part_2_is_correct_test():
-    score = point_receiver_when_outcome_given('input_day_2_test.txt')
+    rounds = parseRounds('input_day_2_test.txt')
+    score = scoreRounds(rounds)
     assert score == 12
 
 
 def test_point_receiver_part_2_is_correct():
-    score = point_receiver_when_outcome_given('input_day_2.txt')
+    rounds = parseRounds('input_day_2.txt')
+    score = scoreRounds(rounds)
     assert score == 10334
-
-
-def test_outcome_converter():
-    raw_move = "A X"
-    outcome = outcome_converter(raw_move[2], "X", "Y", "Z")
-    assert outcome == "lose"
 
