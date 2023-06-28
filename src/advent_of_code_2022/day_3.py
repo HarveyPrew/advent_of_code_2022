e98dd34584b676e0591b9ea3509a2820969d9d92
HarveyPrew <harvey.prewer@gmail.com>
2023-06-28 12:01:07 +0100

    item in both compartments working

--- src/advent_of_code_2022/day_3.py
+++ src/advent_of_code_2022/day_3.py
@@ -1,6 +1,11 @@
 
 def rucksack_splitter(rucksack):
     half_len = len(rucksack) // 2
-    first_compartment = rucksack[:half_len]
-    second_compartment = rucksack[half_len:]
+    first_compartment = list(rucksack[:half_len])
+    second_compartment = list(rucksack[half_len:])
     return first_compartment, second_compartment
+
+
+def recurring_item_finder(first_compartment, second_compartment):
+    recurring_item = list(set(first_compartment) & set(second_compartment))
+    return recurring_item
--- test/test_day3.py
+++ test/test_day3.py
@@ -1,8 +1,17 @@
-from advent_of_code_2022.day_3 import rucksack_splitter
+from advent_of_code_2022.day_3 import rucksack_splitter, recurring_item_finder
 
 
 def test_rucksack_splits():
     rucksack = "vJrw"
     first_compartment, second_compartment = rucksack_splitter(rucksack)
-    assert first_compartment == 'vJ'
-    assert second_compartment == 'rw'
+    assert first_compartment == ['v', 'J']
+    assert second_compartment == ['r', 'w']
+
+
+def test_item_in_both_compartments():
+    first_compartment = ['v', 'J', 'r']
+    second_compartment = ['r', 'w', 'z']
+
+    recurring_item = recurring_item_finder(first_compartment,
+                                           second_compartment)
+    assert recurring_item == ['r']
