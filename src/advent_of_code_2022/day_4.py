16c353d987e2a62513a800f535325deeaf05ff0a
HarveyPrew <harvey.prewer@gmail.com>
2023-07-19 16:33:01 +0100

    got range extractor working and renaming

--- src/advent_of_code_2022/day_4.py
+++ src/advent_of_code_2022/day_4.py
@@ -1,10 +1,18 @@
 
 def parse_assignment_pairs(path):
-    assignments = list()
+    list_of_section_assignment_pairs = list()
     with open(path) as f:
         for line in f:
-            assignment = list(line.strip().partition(','))
-            assignment.pop(1)
-            assignments.append(assignment)
+            section_assignment_pairs = list(line.strip().partition(','))
+            section_assignment_pairs.remove(',')
+            list_of_section_assignment_pairs.append(section_assignment_pairs)
 
-    return assignments
+    return list_of_section_assignment_pairs
+
+
+def range_extractor(section_assignment):
+    assignment_split = list(section_assignment.partition('-'))
+    assignment_split.remove('-')
+    assignment_range = [eval(i) for i in assignment_split]
+    full_assignment_range = list(range(assignment_range[0], assignment_range[1] + 1))
+    return full_assignment_range
--- test/test_day4.py
+++ test/test_day4.py
@@ -1,4 +1,5 @@
-from advent_of_code_2022.day_4 import parse_assignment_pairs
+from advent_of_code_2022.day_4 import (parse_assignment_pairs,
+                                       range_extractor)
 
 
 def test_parse_assignments():
@@ -7,3 +8,11 @@ def test_parse_assignments():
     assert list_of_pairs == [['2-88', '13-89'],
                              ['12-94', '12-94'],
                              ['34-69', '34-61']]
+
+
+def test_string_to_range():
+    string_range = '3-6'
+
+    actual_answer = range_extractor(string_range)
+
+    assert actual_answer == [3, 4, 5, 6]
